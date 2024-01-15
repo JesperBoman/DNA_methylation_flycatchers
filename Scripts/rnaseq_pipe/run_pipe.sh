88ef5ab8a2e91e8f7011947f02c1304d7a48e35e
JesperBoman <74507057+JesperBoman@users.noreply.github.com>
2024-01-15 17:32:09 +0100

    Update run_pipe.sh

--- Scripts/rnaseq_pipe/run_pipe.sh
+++ Scripts/rnaseq_pipe/run_pipe.sh
@@ -8,7 +8,7 @@ NXF_OPTS='-Xms1g -Xmx4g'
 
 
 
-#gffdir="/crex/proj/sllstore2017033/nobackup/work/jesper/DNA_methylation_flycatchers/annotation/maker/run4/fAlb15.maker.output/fAlb15.chrom_run4_featuresOnly.gff"
+
 
 
 nextflow run nf-core/rnaseq -name "Testis" --input testis_rna_sample_sheet.csv -profile uppmax --project "" --max_cpus 20 --max_memory 128GB --fasta ../methylseq_pipe/fAlb15.chrom_Olandfix_hardmask_polyCorG100bpthres_softmask.fa --gtf fAlb15.chrom_run4_featuresOnly.gtf --outdir ./Testis --skip_preseq
